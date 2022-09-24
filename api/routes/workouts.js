b1d1eca4a784b2fc3a96c792d112deb991c9c1ca
zak aden <s.aden159@gmail.com>
2022-09-24 17:42:04 +0100

    set up workout routes

--- api/routes/workouts.js
+++ api/routes/workouts.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const Router = express.Router();
 
-Router.get('/', () => {
-
+// get all workouts
+Router.get('/', (req, res) => {
+  res.json({msg: 'get all workouts'})
 })
 
-module.exports = Router
+// get single workout
+Router.get('/:id', (req, res) => {
+  res.json({msg: 'get single workout'})
+})
 
+// post new workout
+Router.post('/', (req, res) => {
+  res.json({msg: 'post workout'})
+})
+
+// update workout
+Router.patch('/:id', (req, res) => {
+  res.json({msg: 'update workout'})
+})
+
+// del workout 
+Router.delete('/:id', (req, res) => {
+  res.json({msg: 'delete workout'})
+})
+
+module.exports = Router;
--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const workoutRouter = require('./routes/workouts');
 const app = express();
 
 // middleware
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
