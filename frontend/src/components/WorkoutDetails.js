8d1cde017161e08a1de077afb6d0d0335ed2cff2
zak aden <s.aden159@gmail.com>
2022-09-25 18:57:27 +0100

    create workoutDetails component and add styles

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WorkoutDetails = ({ workout: {title, reps, sets, createdAt} }) => {
+  return (
+    <div className='workout-details'>
+      <h4>{title}</h4>
+      <p><strong>Sets:</strong>{sets}</p>
+      <p><strong>Reps:</strong>{reps}</p>
+      <p>{createdAt}</p>
+    </div>
+  )
+}
+
+export default WorkoutDetails;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+import WorkoutDetails from '../components/WorkoutDetails';
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/workouts')
+      .then(({data}) => setWorkouts(data))
+      .catch(err => console.log(err))
+  }, [])
+
+  console.log(workouts);
+
   return (
     <div className='home'>
-      <h2>home</h2>
+      <div className='workouts'>
+        {workouts.length > 0 && workouts.map((workout) => 
+          <WorkoutDetails key={workout._id} workout={workout}/>
+        )}
+      </div>
     </div>
   )
 }
