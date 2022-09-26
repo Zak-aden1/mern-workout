42ae203f5d269bbe2c356503a8ae5594d92533b1
zak aden <s.aden159@gmail.com>
2022-09-26 19:11:18 +0100

    create workoutContextProvider and wrap root app component

---
+++ frontend/src/context/workoutContext.js
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const WorkoutContext = createContext();
+
+export const WorkoutContextProvider = ({ children }) => {
+  return (
+    <WorkoutContext.Provider>
+      {children}
+    </WorkoutContext.Provider>
+  )
+}
