2745e080c9d82f61eac454e0470550ab7f6ab8ac
zak aden <s.aden159@gmail.com>
2022-09-26 20:37:53 +0100

    use library to format date

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React from 'react'
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 /* card to show and delete specific workout */
 const WorkoutDetails = ({ workout: {title, reps, sets, createdAt, _id} }) => {
   const { dispatch } = useWorkoutsContext();
@@ -20,7 +22,7 @@ const WorkoutDetails = ({ workout: {title, reps, sets, createdAt, _id} }) => {
       <h4>{title}</h4>
       <p><strong>Sets: </strong>{sets}</p>
       <p><strong>Reps: </strong>{reps}</p>
-      <p>{createdAt}</p>
+      <p>{formatDistanceToNow(new Date(createdAt), { addSuffix: true })}</p>
       <span className="material-symbols-outlined"  onClick={handleClick}>delete</span>
     </div>
   )
