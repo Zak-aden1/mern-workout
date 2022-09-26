3cd284261e28a11784555597dcf6031c6d63f36f
zak aden <s.aden159@gmail.com>
2022-09-26 19:49:28 +0100

    create workout reducer and hook

--- frontend/src/context/workoutContext.js
+++ frontend/src/context/workoutContext.js
@@ -1,10 +1,32 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const WorkoutContext = createContext();
 
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_WORKOUTS':
+      return {
+        workouts: action.payload
+      }
+    case 'CREATE_WORKOUT':
+      return {
+        workouts: [action.payload, ...state]
+      }
+    case 'DELETE_WORKOUT':
+      return {
+        workouts: []
+      }
+    default:
+      return state
+  }
+}
+
 export const WorkoutContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    workouts: null
+  })
   return (
-    <WorkoutContext.Provider>
+    <WorkoutContext.Provider value={{state, dispatch}}>
       {children}
     </WorkoutContext.Provider>
   )
