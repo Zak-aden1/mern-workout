05f722eeded41266e18559d46472842dc7b9b771
zak aden <s.aden159@gmail.com>
2022-09-28 19:47:26 +0100

    create login method and controller func

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -7,7 +7,17 @@ const createToken = (_id) => {
 }
 
 const loginUser = async (req, res) => {
-  res.json({msg: 'login'})
+  const { email, password } = req.body
+
+  try {
+    const user = await User.login({email, password})
+    // create token
+    const token = createToken(user._id)
+
+    res.status(200).json({email: req.body.email, token})
+    } catch (error) {
+    res.status(200).json({error: error.message})
+  }
 
 };
 
