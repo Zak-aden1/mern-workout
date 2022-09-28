898f4dcbd1d03408f2e69f57d9259d6bf20f297b
zak aden <s.aden159@gmail.com>
2022-09-28 19:14:43 +0100

    create jwt token

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'});
+}
 
 const loginUser = async (req, res) => {
   res.json({msg: 'login'})
@@ -8,7 +14,11 @@ const loginUser = async (req, res) => {
 const signupUser = async (req, res) => {
   try {
     const user = await User.signup(req.body);
-    res.status(200).json({email: req.body.email, user})
+
+    // create token
+    const token = createToken(user._id)
+
+    res.status(200).json({email: req.body.email, token})
   } catch (error) {
     res.status(200).json({error: error.message})
   }
