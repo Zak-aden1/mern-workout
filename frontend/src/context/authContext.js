fe6149c51056f852f51f43f8da5ee05a96afef12
zak aden <s.aden159@gmail.com>
2022-09-28 21:55:55 +0100

    create authReducer for login and logout

--- frontend/src/context/authContext.js
+++ frontend/src/context/authContext.js
@@ -1,11 +1,30 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const AuthContext = createContext();
 
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        user: action.payload
+      }
+    case 'LOGOUT':
+      return {
+        user: null
+      }
+    default:
+      return null
+  }
+}
+
 export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null
+  })
+  console.log('authCOntext state:', state);
 
   return (
-    <AuthContext.Provider>
+    <AuthContext.Provider value={{...state, dispatch}}>
       { children }
     </AuthContext.Provider>
   )
